dff29ad2b6b4c58e452071af7776b682a1c6f7a8
zhangbangtuan123 <48170147+zhangbangtuan123@users.noreply.github.com>
2024-10-24 11:50:59 +0800

    Update EmpServer.java

--- src/main/java/com/yidu/server/EmpServer.java
+++ src/main/java/com/yidu/server/EmpServer.java
@@ -11,7 +11,7 @@ public class EmpServer {
     private EmpMapper empMapper;
 
     public Emp getEmpById(String empno){
-        System.out.println("1111");
+        System.out.println("111retertertrert1");
         return empMapper.selectByPrimaryKey(empno);
     }
 
